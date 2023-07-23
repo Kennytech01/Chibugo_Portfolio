f2103b79aedb30dd51b282d15f99d0f87591ac77
Kennytech01 <Kennysonvictory@gmail.com>
2023-07-23 04:51:55 +0100

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className='md:hidden flex flex-col justify-center items-center max-w-[1040px]  m-auto md:ml-[25%] p-4 py-10 bg-primary-content'>
         <div className=' w-full md:w-1/2 flex flex-wrap justify-evenly items-center py-10 font-light text-stone-100 text-lg'>
-            <a href="#main" className='cursor-pointer hover:scale-110 duration-200 ease-in capitalize'>home</a>
+            <a href="#home" className='cursor-pointer hover:scale-110 duration-200 ease-in capitalize'>home</a>
             <a href="#" className='cursor-pointer hover:scale-110 duration-200 ease-in capitalize'>cookie policy</a>
             <a href="#" className='cursor-pointer hover:scale-110 duration-200 ease-in '>Terms of use</a>
             <a href="#" className='cursor-pointer hover:scale-110 duration-200 ease-in capitalize'>privacy policy</a>
@@ -13,7 +13,7 @@ export const Footer = () => {
         </div>
         <div className='md:flex block flex-wrap justify-around items-center w-full pt-4 '>
             <div>
-                <p className='text-stone-200 font-medium tracking-widest py-4'>Copyright © 2023 - All right reserved by <span className='font-black text-lg'>Kennytech</span> </p>
+                <p className='text-stone-200 font-medium tracking-widest py-4'>Copyright © 2023 - All right reserved by <span className='font-black text-lg'>Chibugo Cynthia .A</span> </p>
             </div>
             <div>
                 <div className="flex md:justify-between justify-evenly w-full text-stone-200 py-4">
