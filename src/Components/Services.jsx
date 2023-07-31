ec6b14a065979ee2223b0b6b5b0f06a3bece315c
Kennytech01 <Kennysonvictory@gmail.com>
2023-07-31 04:11:43 +0100

    ready to Update

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,69 +1,66 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {FiExternalLink} from 'react-icons/fi'
-import { Modal } from './Modal'
-
+import services from './../assets/images/services.jpg'
+import { ServiceData } from './ServiceData' 
+import myImage from './../assets/images/myImage.png'
+import {FaTimes} from 'react-icons/fa'
+import AccountReconciliation from './../assets/images/AccountReconciliation.jpeg'
 
 export const Services = () => {
-    const [openModel, setOpenModal] = useState(false)
+   const [popUp, setPopUp] = useState([]);
+   const [popUpToggle, setPopUpToggle] = useState(false);
+
+
+   const handleClick = (service) => {
+    setPopUp([service])
+    setPopUpToggle(!popUpToggle)
+   }
+
+   useEffect(()=>{
+    const BODY = document.querySelector('body');
+    BODY.style.overflow = popUpToggle? 'hidden' : 'auto';
+   }, [popUpToggle])
 
   return (
-    <div id='services' className='md:ml-[25%] md:w-[75%] max-w-[1040px] py-10'>
+    <div className='md:ml-[25%] md:w-[75%] max-w-[1040px] p-4 py-16'>
         <ul class="capitalize p-5">
             <li class="text-lg font-bold uppercase ">-services</li>
-            <li class="font-extrabold text-3xl py-2 text-primary">my specializations</li>
+            <li class="font-extrabold md:text-4xl text-3xl py-2 text-primary">my specializations</li>
         </ul>
-        <div className='grid lg:grid-cols-3 gap-4 sm:grid-cols-2 p-3 h-full'>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>01</p>
-                    <p className='font-bold text-secondary p-2 text-lg capitalize'>Account Reconciliation/ audit</p>
-                    <p className='p-2 text-start' >Oversee all financial and accounting activities for the organization, including managing and overseeing financial processes and activities</p>
-                    {/* <div> */}
-                        <a href='#services' onClick={()=> setOpenModal(true)} className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                        {/* <Modal open ={openModel} /> */}
-                    {/* </div> */}
-                </div>
-            </div>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>02</p>
-                    <p className='font-bold text-secondary p-2 text-lg capitalize'>Data Entry/ financial Analysis</p>
-                    <p className='p-2 text-start' >Compile, verify accuracy and sort information according to priorities to prepare source data for computer entry</p>
-                    <a href="#" className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                </div>
-            </div>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>03</p>
-                    <p className='font-bold text-secondary p-2 text-lg'>Team Management</p>
-                    <p className='p-2 text-start' >Create guidelines, goals, checkpoints, and objectives for employees to improve productivity while...</p>
-                    <a href="#" className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                </div>
-            </div>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>04</p>
-                    <p className='font-bold text-secondary p-2 text-lg'>Customer Relationship Management (CRM)</p>
-                    <p className='p-2 text-start' >Build and maintain profitable relationships with key customers.Resolve customer complaints quickly and efficiently</p>
-                    <a href="#" className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                </div>
-            </div>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>05</p>
-                    <p className='font-bold text-secondary p-2 text-lg capitalize'>Cost Accounting & sales Management</p>
-                    <p className='p-2 text-start  '>Developing and implementing new cost accounting procedures within your company.This might include anything from...</p>
-                    <a href="#" className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                </div>
-            </div>
-            <div className='p-2 m-1 '>
-                <div className='w-full text-center py-2'>
-                    <p className='font-light p-2 text-start text-lg'>06</p>
-                    <p className='font-bold text-secondary p-2 text-lg capitalize'>analytical skills & financial reporting</p>
-                    <p className='p-2 text-start' >Monitor, review and analyze territory monthly financial packages and prepare required reconciliations and adjustments, including...</p>
-                    <a href="#" className='flex items-center border justify-center btn mx-5'>read more <FiExternalLink className='mx-1'/></a>
-                </div>
-            </div>    
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4'>
+            {
+                ServiceData.map((service)=>{
+                    return (
+                        <div>
+                            <div className=''>
+                                <p className='font-light p-2 text-start text-lg'>{service.number}</p>
+                                <p className='font-bold text-secondary p-2 text-lg capitalize'>{service.title}</p>
+                                <p className='p-2'>{service.duty}</p>
+                                <div className='py-3'>
+                                    <button onClick={()=>handleClick(service)} className='flex items-center border justify-center btn mx-5'>{service.link} <FiExternalLink className='mx-1'/></button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        <div>
+            {popUpToggle && (
+                popUp.map((pop)=>{
+                    return (
+                        <div onClick={()=> handleClick(pop)} className='fixed flex items-center justify-center top-0 left-0 right-0 h-screen w-full bg-black/70 z-[999]'>
+                            <div className='bg-stone-200 w-1/2 h-auto relative shadow-lg  rounded'>
+                                <button onClick={()=>handleClick(pop)} className='text-right absolute right-2 top-2 text-xl font-bold border rounded-full'><FaTimes/></button>
+                                <div className='p-5 flex flex-col justify-center items-center m-auto' >
+                                    <img src={pop.Image} alt="" className='p-2 rounded-2xl ' />
+                                    <p className='p-5 font-light first-letter:text-xl first-letter:mr-1 first-letter:font-bold'>{pop.details}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }))
+            }
         </div>
     </div>
   )
